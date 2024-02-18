d21e675468968ae5c085f739b81f4b3ca66bfe58
gokuloganathan <skillgallery75@gmail.com>
2024-02-18 17:25:03 +0530

    component chnages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HydratedComponentComponent } from "./hydrated-component/hydrated-component.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, HydratedComponentComponent]
 })
 export class AppComponent {
   title = 'AngularV17.1-sandbox';
